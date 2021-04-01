ee7ed9f6e50dd46ae62825b5474d647582b2fc9b
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-03-31 19:58:32 -0700

    Initial Commit

--- app/src/main/java/com/example/foodrecipes/MainActivity.kt
+++ app/src/main/java/com/example/foodrecipes/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.foodrecipes
 
 import android.os.Bundle
+import android.view.Gravity
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.foodrecipes.ui.theme.FoodRecipesTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,11 +27,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             FoodRecipesTheme {
 
-                
             }
         }
     }
 }
+
 
 @Preview
 @Composable
