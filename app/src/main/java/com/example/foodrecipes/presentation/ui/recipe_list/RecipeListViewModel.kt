8e955a7c5276fdd00fbeebd1dff50f47c5c3d121
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-04-01 14:36:37 -0700

    Feat: Added a view model for RecipeListFragment

---
+++ app/src/main/java/com/example/foodrecipes/presentation/ui/recipe_list/RecipeListViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.foodrecipes.presentation.ui.recipe_list
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class RecipeListViewModel @Inject constructor(
+): ViewModel() {
+}
