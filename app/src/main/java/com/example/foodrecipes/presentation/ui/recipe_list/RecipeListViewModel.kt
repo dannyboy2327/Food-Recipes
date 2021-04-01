aeed91476c4e10975c6ae151f282507fe99f92b1
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-04-01 16:26:36 -0700

    Fix: changed mutableLiveData to mutableState

--- app/src/main/java/com/example/foodrecipes/presentation/ui/recipe_list/RecipeListViewModel.kt
+++ app/src/main/java/com/example/foodrecipes/presentation/ui/recipe_list/RecipeListViewModel.kt
@@ -1,8 +1,15 @@
 package com.example.foodrecipes.presentation.ui.recipe_list
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.foodrecipes.domain.model.Recipe
 import com.example.foodrecipes.repository.RecipeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Named
 
@@ -11,4 +18,17 @@ class RecipeListViewModel @Inject constructor(
     private val repository: RecipeRepository,
     @Named("auth_token") private val token: String
 ): ViewModel() {
+
+    val recipes: MutableState<List<Recipe>> = mutableStateOf(listOf())
+
+    init {
+        viewModelScope.launch {
+            val result = repository.search(
+                token = token,
+                page = 1,
+                query = "chicken"
+            )
+            recipes.value = result
+        }
+    }
 }
