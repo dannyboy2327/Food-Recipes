acb63f9d69b4309dbea0a5575a548102cb9be408
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-04-06 22:27:39 -0700

    Feat: added a function to change app theme on click

--- app/src/main/java/com/example/foodrecipes/presentation/components/SearchAppBar.kt
+++ app/src/main/java/com/example/foodrecipes/presentation/components/SearchAppBar.kt
@@ -10,9 +10,12 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,6 +24,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.foodrecipes.presentation.ui.recipe_list.FoodCategory
 import com.example.foodrecipes.presentation.ui.recipe_list.getAllFoodCategories
 import kotlinx.coroutines.launch
@@ -34,11 +38,12 @@ fun SearchAppBar(
     scrollPosition: Int,
     selectedCategory: FoodCategory?,
     onSelectedCategoryChanged: (String) -> Unit,
-    onChangeCategoryScrollPosition: (Int) -> Unit
+    onChangeCategoryScrollPosition: (Int) -> Unit,
+    onToggleTheme: () -> Unit
 ) {
     Surface(
         modifier = Modifier.fillMaxWidth(),
-        color = Color.White,
+        color = MaterialTheme.colors.surface,
         elevation = 8.dp,
     ) {
         Column {
@@ -67,9 +72,29 @@ fun SearchAppBar(
                         onExecuteSearch()
                         keyboardController?.hideSoftwareKeyboard()
                     }),
-                    textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
+                    textStyle = MaterialTheme.typography.button,
                     colors = TextFieldDefaults.textFieldColors(backgroundColor = MaterialTheme.colors.surface)
                 )
+                ConstraintLayout(
+                    modifier = Modifier
+                        .align(CenterVertically)
+                ) {
+                    val menu = createRef()
+                    IconButton(
+                        onClick = { onToggleTheme() },
+                        modifier = Modifier
+                            .constrainAs(menu) {
+                                end.linkTo(parent.end)
+                                top.linkTo(parent.top)
+                                bottom.linkTo(parent.bottom)
+                            }
+                        ) {
+                        Icon(
+                            imageVector = Icons.Filled.MoreVert,
+                            contentDescription = null,
+                        )
+                    }
+                }
             }
 
             val scrollState = rememberScrollState()
