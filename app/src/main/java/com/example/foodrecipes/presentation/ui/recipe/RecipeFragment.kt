a5d413691335710cb7ec5cb0b444736936c6f926
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-04-08 13:46:50 -0700

    Feat: added composable to the fragment

--- app/src/main/java/com/example/foodrecipes/presentation/ui/recipe/RecipeFragment.kt
+++ app/src/main/java/com/example/foodrecipes/presentation/ui/recipe/RecipeFragment.kt
@@ -5,7 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.TextStyle
@@ -14,10 +19,34 @@ import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.example.foodrecipes.presentation.BaseApplication
+import com.example.foodrecipes.presentation.components.CircularIndeterminateProgressBar
+import com.example.foodrecipes.presentation.components.DefaultSnackbar
+import com.example.foodrecipes.presentation.components.RecipeView
+import com.example.foodrecipes.presentation.components.util.SnackbarController
+import com.example.foodrecipes.presentation.theme.AppTheme
+import com.example.foodrecipes.presentation.ui.recipe.RecipeEvent.GetRecipeEvent
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class RecipeFragment: Fragment() {
+
+    @Inject
+    lateinit var application: BaseApplication
+
+    private val snackbarController = SnackbarController(lifecycleScope)
+
+    private val viewModel: RecipeViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.getInt("recipeId")?.let { recipeId ->
+            viewModel.onTriggerEvent(GetRecipeEvent(recipeId))
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,10 +55,53 @@ class RecipeFragment: Fragment() {
     ): View {
         return ComposeView(requireContext()).apply {
             setContent {
-                Column(modifier = Modifier.padding(16.dp)) {
-                    Text(
-                        text = "This is Recipe Fragment",
-                    )
+
+                val loading = viewModel.loading.value
+
+                val recipe = viewModel.recipe.value
+
+                val scaffoldState = rememberScaffoldState()
+
+                AppTheme(darkTheme = application.isDark.value) {
+                    Scaffold(
+                        scaffoldState = scaffoldState,
+                        snackbarHost = {
+                            scaffoldState.snackbarHostState
+                        }
+                    ){
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                        ) {
+                            if (loading && recipe == null) {
+                                Text(text = "Loading...")
+                            } else {
+                                recipe?.let {
+                                    if (it.id == 1) {
+                                        snackbarController.showSnackbar(
+                                            scaffoldState = scaffoldState,
+                                            message = "An error occurred with this recipe",
+                                            actionLabel = "Ok"
+                                        )
+                                    } else {
+                                        RecipeView(recipe = recipe)
+                                    }
+                                }
+                            }
+                            CircularIndeterminateProgressBar(
+                                isDisplayed = loading,
+                                verticalBias = 0.3f,
+                            )
+                            DefaultSnackbar(
+                                snackbarHostState = scaffoldState.snackbarHostState,
+                                onDismiss = {
+                                    scaffoldState.snackbarHostState.currentSnackbarData?.dismiss()
+                                },
+                                modifier = Modifier
+                                    .align(Alignment.BottomCenter)
+                            )
+                        }
+                    }
                 }
             }
         }
